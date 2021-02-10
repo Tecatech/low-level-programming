776f93af420c90a80422a946732de84ea7beb772
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2021-02-10 22:49:00 +0300

    resources added

---
+++ warm-up/main.s
@@ -0,0 +1,19 @@
+.global _start
+.section .text
+
+_start:
+    # ssize_t write(unsigned int fd, const void *buf, size_t count);
+    mov x8, #64
+    mov x0, #1
+    ldr x1, = message
+    mov x2, 10
+    svc 0
+    
+    # void exit(int status);
+    mov x8, #0x5d
+    mov x0, #0x41
+    svc 0
+
+.section .data
+    message:
+    .ascii "Good job!\n"
