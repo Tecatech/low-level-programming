38e62aef645d9fc70e71d4deb4e7448da23f4ef8
Dmitriy Rodionov <57829161+Tecatech@users.noreply.github.com>
2021-03-24 13:21:08 +0300

    resources added

--- warm-up/main.s
+++ warm-up/main.s
@@ -1,19 +1,24 @@
-.global _start
-.section .text
-
+    .arch armv8-a
+    
+    .data
+    .align  3
+msg:
+    .ascii  "Good job!\n"
+    
+    .text
+    .align  2
+    .global _start
+    .type   _start, %function
 _start:
     # ssize_t write(unsigned int fd, const void *buf, size_t count);
-    mov x8, #64
-    mov x0, #1
-    ldr x1, = message
-    mov x2, 10
-    svc 0
+    mov     x0, #1
+    mov     x8, #64
+    ldr     x1, = msg
+    mov     x2, #10
+    svc     #0
     
     # void exit(int status);
-    mov x8, #0x5d
-    mov x0, #0x41
-    svc 0
-
-.section .data
-message:
-    .ascii "Good job!\n"
+    mov     x0, #0
+    mov     x8, #93
+    svc     #0
+    .size   _start, .-_start
