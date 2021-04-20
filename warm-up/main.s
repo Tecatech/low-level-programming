14d3ad83f9455682f33f4d82600358d463043b9d
Dmitriy Rodionov <57829161+Tecatech@users.noreply.github.com>
2021-04-20 11:39:42 +0300

    resources added

--- warm-up/main.s
+++ warm-up/main.s
@@ -10,14 +10,11 @@ msg:
     .global _start
     .type   _start, %function
 _start:
-    # ssize_t write(unsigned int fd, const void *buf, size_t count);
     mov     x0, #1
     mov     x8, #64
     ldr     x1, = msg
     mov     x2, #10
     svc     #0
-    
-    # void exit(int status);
     mov     x0, #0
     mov     x8, #93
     svc     #0
