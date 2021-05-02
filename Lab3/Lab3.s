67c4456a9fbc9aa1d843ef742270068231b71f7c
dima <hexdump1337@gmail.com>
2021-05-02 13:16:00 +0300

    resources added

--- Lab3/Lab3.s
+++ Lab3/Lab3.s
@@ -52,11 +52,14 @@ _start:
     .align  2
     .equ    filename, 16
     .equ    fd, 24
-    .equ    buf, 32
-    .equ    res, 1056
-    .equ    numlen, 2080
+    .equ    wordlen, 32
+    .equ    buf, 36
+    .equ    buflen, 32
+    .equ    res, buf + buflen
+    .equ    reslen, buflen * 2 + 1
+    .equ    framelen, res + reslen
 work:
-    mov     x16, #2084
+    mov     x16, framelen
     sub     sp, sp, x16
     stp     x29, x30, [sp]
     mov     x29, sp
@@ -69,18 +72,25 @@ work:
     cmp     x0, #0
     bge     0f
     bl      writeerr
-    b       10f
+    b       14f
 0:
     str     x0, [x29, fd]
-    mov     w3, #10
+    add     x4, x29, res
+    mov     x5, x4
+    add     x6, x29, wordlen
+    mov     x7, x6
+    mov     w9, #10
+    mov     w10, #0
+    mov     w11, #0
 1:
     ldr     x0, [x29, fd]
     add     x1, x29, buf
-    mov     x2, #1024
+    mov     x2, buflen
     mov     x8, #63
     svc     #0
+    mov     w12, #0
     cmp     x0, #0
-    beq     9f
+    beq     12f
     bgt     2f
     str     x0, [sp, #-8]!
     ldr     x0, [x29, fd]
@@ -89,65 +99,81 @@ work:
     ldr     x0, [sp], #8
     bl      writeerr
     mov     x0, #1
-    b       10f
+    b       14f
 2:
     add     x1, x29, buf
-    strb    wzr, [x1, x0]
-    add     x4, x29, res
-    mov     x5, x4
-    add     x6, x29, numlen
-    mov     x7, x6
 3:
-    ldrb    w0, [x1], #1
-    cbz     w0, 8f
-    cmp     w0, ' '
-    beq     3b
-    mov     w9, #0
+    ldrb    w13, [x1], #1
+    add     w12, w12, #1
+    cmp     w13, ' '
+    beq     4f
     cmp     x4, x5
-    beq     5f
+    beq     7f
+    cbnz    w10, 7f
+    b       5f
 4:
-    mov     w10, ' '
-    strb    w10, [x4], #1
-    mov     x6, x7
-    cbnz    w9, 6f
+    cmp     w0, w12
+    beq     11f
+    cbz     w10, 3b
 5:
-    strb    w0, [x4], #1
-    add     w9, w9, #1
-    ldrb    w0, [x1], #1
-    cbz     w0, 4b
-    cmp     w0, ' '
-    bne     5b
-    b       4b
+    mov     w14, ' '
+    strb    w14, [x4], #1
+    mov     x6, x7
+    cbnz    w10, 9f
+    b       7f
 6:
-    udiv    w11, w9, w3
-    msub    w10, w3, w11, w9
-    add     w10, w10, '0'
-    strb    w10, [x6], #1
-    mov     w9, w11
-    cbnz    w9, 6b
+    ldrb    w13, [x1], #1
+    add     w12, w12, #1
+    cmp     w13, ' '
+    beq     5b
 7:
-    ldrb    w10, [x6, #-1]!
-    strb    w10, [x4], #1
-    cmp     x6, x7
-    bne     7b
-    cbnz    w0, 3b
+    strb    w13, [x4], #1
+    add     w10, w10, #1
+    cmp     w0, w12
+    bne     6b
 8:
-    mov     w0, '\n'
-    strb    w0, [x4], #1
+    cmp     w12, buflen
+    blt     5b
+    b       11f
+9:
+    udiv    w15, w10, w9
+    msub    w14, w9, w15, w10
+    add     w14, w14, '0'
+    strb    w14, [x6], #1
+    mov     w10, w15
+    cbnz    w10, 9b
+10:
+    ldrb    w14, [x6, #-1]!
+    strb    w14, [x4], #1
+    cmp     x6, x7
+    bne     10b
+    cmp     w0, w12
+    bne     3b
+11:
     mov     x0, #1
     add     x1, x29, res
+    add     x1, x1, x11
     sub     x2, x4, x1
     mov     x8, #64
     svc     #0
+    add     x11, x2, x11
     b       1b
-9:
+12:
+    cbnz    w10, 5b
+13:
+    mov     w14, '\n'
+    strb    w14, [x1]
+    mov     x0, #1
+    mov     x2, #1
+    mov     x8, #64
+    svc     #0
     ldr     x0, [x29, fd]
     mov     x8, #57
     svc     #0
     mov     x0, #0
-10:
+14:
     ldp     x29, x30, [sp]
-    mov     x16, #2084
+    mov     x16, framelen
     add     sp, sp, x16
     ret
     .size   work, . - work
